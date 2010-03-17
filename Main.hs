724e0f69946889525803e3f06f25db34290c2d2b
Simon Hengel <simon.hengel@wiktory.org>
2010-03-17 21:46:45 +0100

    Haddock is now used for parsing

--- Main.hs
+++ Main.hs
@@ -1,12 +1,44 @@
 module Main where
 
-import System.Environment
-import Test.DocTest
-import Test.DocTest.Parser
-import Test.HUnit
+import System.Environment   ( getArgs )
+import Test.DocTest.Parser  ( parseModule )
+import Test.DocTest         ( DocTest(..)
+                            , docTestToTestCase
+                            )
+import Test.HUnit           ( runTestTT
+                            , Test(..)
+                            )
+import Documentation.Haddock.DocTest  (
+                              DocTestAsset(..)
+                            , Example(..)
+                            , getTestAssets
+                            )
+import Data.Char
+
+-- | Remove all leading and trailing whitespace
+strip = reverse . dropWhile isSpace . reverse . dropWhile isSpace
+
+docTestFromAsset :: DocTestAsset -> [DocTest]
+docTestFromAsset asset = map tranform (testList asset)
+  where
+    tranform :: Example -> DocTest
+    tranform (Example p e r) = DocTest
+            { source = sourceFile asset
+            , _module = moduleName asset
+            , expression = e
+            , result = strip $ unlines r
+            }
+
+internalParse args = do
+  docTests <- mapM parseModule args
+  return $ concat docTests
+
+haddockParse args = do
+  testAssets <- getTestAssets args
+  return $ concat $ map docTestFromAsset testAssets
 
 main = do
-	args <- getArgs
-	docTests <- mapM parseModule args
-	tests <- mapM docTestToTestCase (concat docTests)
-	runTestTT (TestList tests)
+  args <- getArgs
+  docTests <- haddockParse args
+  tests <- mapM docTestToTestCase docTests
+  runTestTT (TestList tests)
