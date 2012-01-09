4c50b29622ea8d7997f2edf2d89668382f75b3d7
Simon Hengel <sol@typeful.net>
2012-01-09 19:35:54 +0100

    Remove duplicate handling of <BLANKLINE>
    This is done in the parser now.

--- src/DocTest.hs
+++ src/DocTest.hs
@@ -36,9 +36,4 @@ toAssertion repl test = do
         exampleResult $ lines result'
       where
         exampleExpression = expression x
-        exampleResult     = map subBlankLines $ result x
-
-        -- interpret lines that only contain the string "<BLANKLINE>" as an
-        -- empty line
-        subBlankLines "<BLANKLINE>" = ""
-        subBlankLines line          = line
+        exampleResult     = result x
