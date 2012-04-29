6cf9f084a9432461742f4e44a97ac82692566d7f
Simon Hengel <sol@typeful.net>
2012-04-29 13:23:11 +0200

    Integrate selftest.sh into Spec.hs

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,6 +2,8 @@ module Main (main) where
 
 import           Test.Hspec.ShouldBe
 
+import           Run (doctest)
+
 import qualified UtilSpec
 import qualified LocationSpec
 import qualified ExtractSpec
@@ -12,12 +14,21 @@ import qualified RunSpec
 import qualified MainSpec
 
 main :: IO ()
-main = hspecX $ do
-  describe "UtilSpec"         UtilSpec.spec
-  describe "LocationSpec"     LocationSpec.spec
-  describe "ExtractSpec"      ExtractSpec.spec
-  describe "ParseSpec"        ParseSpec.spec
-  describe "InterpreterSpec"  InterpreterSpec.spec
-  describe "ReportSpec"       ReportSpec.spec
-  describe "RunSpec"          RunSpec.spec
-  describe "MainSpec"         MainSpec.spec
+main = do
+  doctest [
+      "--optghc=-packageghc"
+    , "--optghc=-isrc"
+    , "--optghc=-idist/build/autogen/"
+    , "--optghc=-optP-include"
+    , "--optghc=-optPdist/build/autogen/cabal_macros.h"
+    , "src/Main.hs"
+    ]
+  hspecX $ do
+    describe "UtilSpec"         UtilSpec.spec
+    describe "LocationSpec"     LocationSpec.spec
+    describe "ExtractSpec"      ExtractSpec.spec
+    describe "ParseSpec"        ParseSpec.spec
+    describe "InterpreterSpec"  InterpreterSpec.spec
+    describe "ReportSpec"       ReportSpec.spec
+    describe "RunSpec"          RunSpec.spec
+    describe "MainSpec"         MainSpec.spec
--- test/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -o nounset
-set -o errexit
-
-cd "`dirname $0`/.."
-
-runhaskell -isrc -itest -packageghc test/Spec.hs
-./test/selftest.sh
