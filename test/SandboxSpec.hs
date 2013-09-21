64844aca3fca27d08f88fe5c5f34e6c9c34ae143
Kazu Yamamoto <kazu@iij.ad.jp>
2013-09-21 12:49:31 +0900

    adding tests for Sandbox.

---
+++ test/SandboxSpec.hs
@@ -0,0 +1,15 @@
+module SandboxSpec where
+
+import Test.Hspec
+
+import Sandbox
+
+spec :: Spec
+spec = do
+  describe "getPackageDbDir" $ do
+      it "parses a config file and extracts package db" $ do
+          pkgDb <- getPackageDbDir "test/sandbox/cabal.sandbox.config"
+          pkgDb `shouldBe` "/home/me/doctest-haskell/.cabal-sandbox/i386-osx-ghc-7.6.3-packages.conf.d"
+
+      it "throws an error if a config file is broken" $ do
+          getPackageDbDir "test/sandbox/bad.config" `shouldThrow` anyException
