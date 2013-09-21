3fae13c3601211bacc8b0655fb7cbd1dfd53a8c3
Kazu Yamamoto <kazu@iij.ad.jp>
2013-09-21 12:49:59 +0900

    refactoring Sandbox.

--- src/Sandbox.hs
+++ src/Sandbox.hs
@@ -1,17 +1,14 @@
-module Sandbox (getSandboxArguments) where
+module Sandbox (getSandboxArguments, getPackageDbDir) where
 
 import Control.Applicative ((<$>))
+import Control.Exception as E (catch, SomeException, throwIO)
 import Data.Char (isSpace)
 import Data.List (isPrefixOf, tails)
-import System.Directory (getCurrentDirectory, doesFileExist, doesDirectoryExist)
+import System.Directory (getCurrentDirectory, doesFileExist)
 import System.FilePath ((</>), takeDirectory, takeFileName)
-import Control.Exception (handle, SomeException)
 
 configFile :: String
 configFile = "cabal.sandbox.config"
-
-sandboxDir :: String
-sandboxDir = ".cabal-sandbox"
 
 pkgDbKey :: String
 pkgDbKey = "package-db:"
@@ -20,54 +17,52 @@ pkgDbKeyLen :: Int
 pkgDbKeyLen = length pkgDbKey
 
 getSandboxArguments :: IO [String]
-getSandboxArguments = do
-    mdir <- getCurrentDirectory >>= getSandboxDir
-    case mdir of
-        Nothing           -> return []
-        Just (sdir,sconf) -> sandboxArguments sdir sconf
-
-getSandboxDir :: FilePath -> IO (Maybe (FilePath,FilePath))
-getSandboxDir dir = do
-    exist <- doesSandboxExist dir
-    if exist then
-        return $ Just (dir </> sandboxDir, dir </> configFile)
-      else do
-        let dir' = takeDirectory dir
-        if dir == dir' then
-            return Nothing
-          else
-            getSandboxDir dir'
-
-doesSandboxExist :: FilePath -> IO Bool
-doesSandboxExist dir = do
-    fileExist <- doesFileExist $ dir </> configFile
-    dirExist <- doesDirectoryExist $ dir </> sandboxDir
-    return (fileExist && dirExist)
-
-sandboxArguments :: FilePath -> FilePath -> IO [String]
-sandboxArguments sdir sconf = handle handler $ do
-    pkgDb <- getPackageDbDir sconf
-    let ver = extractGhcVer pkgDb
-    let (pkgDbOpt,noUserPkgDbOpt)
-          | ver < 706 = ("-package-conf","-no-user-package-conf")
-          | otherwise = ("-package-db",  "-no-user-package-db")
-        pkgDbPath = sdir </> pkgDb
-        libPath   = sdir </> "lib"
-        impOpt = "-i" ++ libPath
-    return [noUserPkgDbOpt, pkgDbOpt, pkgDbPath, impOpt]
+getSandboxArguments = (sandboxArguments <$> getPkgDb) `E.catch` handler
   where
+    getPkgDb = getCurrentDirectory >>= getSandboxConfigFile >>= getPackageDbDir
     handler :: SomeException -> IO [String]
     handler _ = return []
 
+-- | Find a sandbox config file by tracing ancestor directories.
+--   Exception is thrown if not found
+getSandboxConfigFile :: FilePath -> IO FilePath
+getSandboxConfigFile dir = do
+    let cfile = dir </> configFile
+    exist <- doesFileExist cfile
+    if exist then
+        return cfile
+      else do
+        let dir' = takeDirectory dir
+        if dir == dir' then
+            throwIO $ userError "sandbox config file not found"
+          else
+            getSandboxConfigFile dir'
+
+-- | Extract a package db directory from the sandbox config file.
+--   Exception is thrown if the sandbox config file is broken.
 getPackageDbDir :: FilePath -> IO FilePath
-getPackageDbDir sconf = do
-    ls <- lines <$> readFile sconf
-    let [target] = filter ("package-db:" `isPrefixOf`) ls
-    return $ extractValue target
+getPackageDbDir sconf = extractValue . parse <$> readFile sconf
   where
+    parse = head . filter ("package-db:" `isPrefixOf`) . lines
     extractValue = fst . break isSpace . dropWhile isSpace . drop pkgDbKeyLen
 
+-- | Adding necessary GHC options to the package db.
+--   Exception is thrown if the string argument is incorrect.
+--
+-- >>> sandboxArguments "/foo/bar/i386-osx-ghc-7.6.3-packages.conf.d"
+-- ["-no-user-package-db","-package-db","/foo/bar/i386-osx-ghc-7.6.3-packages.conf.d"]
+-- >>> sandboxArguments "/foo/bar/i386-osx-ghc-7.4.1-packages.conf.d"
+-- ["-no-user-package-conf","-package-conf","/foo/bar/i386-osx-ghc-7.4.1-packages.conf.d"]
+sandboxArguments :: FilePath -> [String]
+sandboxArguments pkgDb = [noUserPkgDbOpt, pkgDbOpt, pkgDb]
+  where
+    ver = extractGhcVer pkgDb
+    (pkgDbOpt,noUserPkgDbOpt)
+      | ver < 706 = ("-package-conf","-no-user-package-conf")
+      | otherwise = ("-package-db",  "-no-user-package-db")
+
 -- | Extracting GHC version from the path of package db.
+--   Exception is thrown if the string argument is incorrect.
 --
 -- >>> extractGhcVer "/foo/bar/i386-osx-ghc-7.6.3-packages.conf.d"
 -- 706
