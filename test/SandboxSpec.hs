2f27c5aaf11eb248ae4d89d29d0a1ad4b4efa4ea
Simon Hengel <sol@typeful.net>
2014-07-24 10:14:36 +0800

    Whitespace change

--- test/SandboxSpec.hs
+++ test/SandboxSpec.hs
@@ -4,12 +4,15 @@ import Test.Hspec
 
 import Sandbox
 
+main :: IO ()
+main = hspec spec
+
 spec :: Spec
 spec = do
   describe "getPackageDbDir" $ do
-      it "parses a config file and extracts package db" $ do
-          pkgDb <- getPackageDbDir "test/sandbox/cabal.sandbox.config"
-          pkgDb `shouldBe` "/home/me/doctest-haskell/.cabal-sandbox/i386-osx-ghc-7.6.3-packages.conf.d"
+    it "parses a config file and extracts package db" $ do
+      pkgDb <- getPackageDbDir "test/sandbox/cabal.sandbox.config"
+      pkgDb `shouldBe` "/home/me/doctest-haskell/.cabal-sandbox/i386-osx-ghc-7.6.3-packages.conf.d"
 
-      it "throws an error if a config file is broken" $ do
-          getPackageDbDir "test/sandbox/bad.config" `shouldThrow` anyException
+    it "throws an error if a config file is broken" $ do
+      getPackageDbDir "test/sandbox/bad.config" `shouldThrow` anyException
