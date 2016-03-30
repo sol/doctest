b7b5d570c257ac10bdee65e4996f30fcc4c217c8
Simon Hengel <sol@typeful.net>
2016-03-30 18:59:08 +0800

    Whitespace change

--- test/UtilSpec.hs
+++ test/UtilSpec.hs
@@ -9,7 +9,6 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-
   describe "convertDosLineEndings" $ do
     it "converts CRLF to LF" $ do
       convertDosLineEndings "foo\r\nbar\r\nbaz" `shouldBe` "foo\nbar\nbaz"
