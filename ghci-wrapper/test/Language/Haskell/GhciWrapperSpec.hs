d7c2ba6f2d4fd7c6f929539b516625e7f442fae1
Simon Hengel <sol@typeful.net>
2017-07-25 15:19:57 +0800

    Refactoring

--- ghci-wrapper/test/Language/Haskell/GhciWrapperSpec.hs
+++ ghci-wrapper/test/Language/Haskell/GhciWrapperSpec.hs
@@ -1,6 +1,4 @@
 {-# LANGUAGE CPP #-}
-{-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE FlexibleContexts #-}
 module Language.Haskell.GhciWrapperSpec (main, spec) where
 
 import           Test.Hspec
@@ -16,13 +14,10 @@ main :: IO ()
 main = hspec spec
 
 withInterpreterConfig :: Config -> (Interpreter -> IO a) -> IO a
-withInterpreterConfig config action = bracket (Interpreter.new config []) Interpreter.close $ \ghci -> action ghci
+withInterpreterConfig config = bracket (Interpreter.new config []) Interpreter.close
 
 withInterpreter :: ((String -> IO String) -> IO a) -> IO a
-withInterpreter action = withInterpreterConfig defaultConfig $ \ghci -> action (Interpreter.eval ghci)
-
-shouldEvaluateTo :: (HasCallStack, Show a, Eq a) => IO a -> a -> IO ()
-action `shouldEvaluateTo` expected = action >>= flip shouldBe expected
+withInterpreter action = withInterpreterConfig defaultConfig $ action . Interpreter.eval
 
 spec :: Spec
 spec = do
@@ -39,50 +34,50 @@ spec = do
 
   describe "eval" $ do
     it "shows literals" $ withInterpreter $ \ghci -> do
-      ghci "23" `shouldEvaluateTo` "23\n"
+      ghci "23" `shouldReturn` "23\n"
 
     it "shows string literals containing Unicode" $ withInterpreter $ \ghci -> do
-      ghci "\"λ\"" `shouldEvaluateTo` "\"\\955\"\n"
+      ghci "\"λ\"" `shouldReturn` "\"\\955\"\n"
 
     it "evaluates simple expressions" $ withInterpreter $ \ghci -> do
-      ghci "23 + 42" `shouldEvaluateTo` "65\n"
+      ghci "23 + 42" `shouldReturn` "65\n"
 
     it "supports let bindings" $ withInterpreter $ \ghci -> do
-      ghci "let x = 10" `shouldEvaluateTo` ""
-      ghci "x" `shouldEvaluateTo` "10\n"
+      ghci "let x = 10" `shouldReturn` ""
+      ghci "x" `shouldReturn` "10\n"
 
     it "allows import statements" $ withInterpreter $ \ghci -> do
-      ghci "import Data.Maybe" `shouldEvaluateTo` ""
-      ghci "fromJust (Just 20)" `shouldEvaluateTo` "20\n"
+      ghci "import Data.Maybe" `shouldReturn` ""
+      ghci "fromJust (Just 20)" `shouldReturn` "20\n"
 
     it "captures stdout" $ withInterpreter $ \ghci -> do
-      ghci "putStr \"foo\"" `shouldEvaluateTo` "foo"
+      ghci "putStr \"foo\"" `shouldReturn` "foo"
 
     it "captures stdout (Unicode)" $ withInterpreter $ \ghci -> do
-      ghci "putStrLn \"λ\"" `shouldEvaluateTo` "λ\n"
+      ghci "putStrLn \"λ\"" `shouldReturn` "λ\n"
 
     it "captures stdout (empty line)" $ withInterpreter $ \ghci -> do
-      ghci "putStrLn \"\"" `shouldEvaluateTo` "\n"
+      ghci "putStrLn \"\"" `shouldReturn` "\n"
 
     it "captures stdout (multiple lines)" $ withInterpreter $ \ghci -> do
       ghci "putStrLn \"foo\" >> putStrLn \"bar\" >> putStrLn \"baz\""
-        `shouldEvaluateTo` "foo\nbar\nbaz\n"
+        `shouldReturn` "foo\nbar\nbaz\n"
 
     it "captures stderr" $ withInterpreter $ \ghci -> do
-      ghci "import System.IO" `shouldEvaluateTo` ""
-      ghci "hPutStrLn stderr \"foo\"" `shouldEvaluateTo` "foo\n"
+      ghci "import System.IO" `shouldReturn` ""
+      ghci "hPutStrLn stderr \"foo\"" `shouldReturn` "foo\n"
 
     it "captures stderr (Unicode)" $ withInterpreter $ \ghci -> do
-      ghci "import System.IO" `shouldEvaluateTo` ""
-      ghci "hPutStrLn stderr \"λ\"" `shouldEvaluateTo` "λ\n"
+      ghci "import System.IO" `shouldReturn` ""
+      ghci "hPutStrLn stderr \"λ\"" `shouldReturn` "λ\n"
 
     it "shows exceptions" $ withInterpreter $ \ghci -> do
-      ghci "import Control.Exception" `shouldEvaluateTo` ""
-      ghci "throwIO DivideByZero" `shouldEvaluateTo` "*** Exception: divide by zero\n"
+      ghci "import Control.Exception" `shouldReturn` ""
+      ghci "throwIO DivideByZero" `shouldReturn` "*** Exception: divide by zero\n"
 
     it "shows exceptions (ExitCode)" $ withInterpreter $ \ghci -> do
-      ghci "import System.Exit" `shouldEvaluateTo` ""
-      ghci "exitWith $ ExitFailure 10" `shouldEvaluateTo` "*** Exception: ExitFailure 10\n"
+      ghci "import System.Exit" `shouldReturn` ""
+      ghci "exitWith $ ExitFailure 10" `shouldReturn` "*** Exception: ExitFailure 10\n"
 
     it "gives an error message for identifiers that are not in scope" $ withInterpreter $ \ghci -> do
 #if __GLASGOW_HASKELL__ >= 800
