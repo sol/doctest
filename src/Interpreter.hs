5d997cf6d59e6cc26ff8125fd9b7a9c6fe4f05ec
Takano Akio <tak@anoak.io>
2019-06-18 05:53:10 +0000

    Let internal GHC options override user-supplied ones
    This is necessary for doctest to operate correctly when the user passes
    GHC flags like -fdiagnostics-color=always to it.

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -53,13 +53,13 @@ withInterpreter
   -> IO a                   -- ^ Result of action
 withInterpreter flags action = do
   let
-    args = [
+    args = flags ++ [
         "--interactive"
 #if __GLASGOW_HASKELL__ >= 802
       , "-fdiagnostics-color=never"
       , "-fno-diagnostics-show-caret"
 #endif
-      ] ++ flags
+      ]
   bracket (new defaultConfig{configGhci = ghc} args) close action
 
 -- | Evaluate an expression; return a Left value on exceptions.
