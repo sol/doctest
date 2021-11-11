ae1b9832eec4be93f59c801941aee7b72906e749
Andreas Abel <andreas.abel@ifi.lmu.de>
2021-11-11 09:37:49 +0100

    [ #307 ] remove include"cabal_macros.h" from test/doctests.hs
    This makes `cabal test` succeed (for v2-cabal).

--- test/doctests.hs
+++ test/doctests.hs
@@ -7,8 +7,6 @@ main = doctest [
     "-packageghc"
   , "-isrc"
   , "-ighci-wrapper/src"
-  , "-optP-include"
-  , "-optPdist/build/autogen/cabal_macros.h"
   , "src/Run.hs"
   , "src/PackageDBs.hs"
   ]
