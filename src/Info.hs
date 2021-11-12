3c36dfcb2533dd4a18c24fdffb140d89b60eb5f7
Simon Hengel <sol@typeful.net>
2021-11-13 05:19:36 +0700

    Include `ghc --info` output in `--info`

--- src/Info.hs
+++ src/Info.hs
@@ -1,6 +1,52 @@
-module Info where
+{-# LANGUAGE CPP #-}
+module Info (
+  versionInfo
+, info
+#ifdef TEST
+, formatInfo
+#endif
+) where
 
-import           Data.List
+import           Data.List.Compat
+import           System.Process
+import           System.IO.Unsafe
 
-formatInfo :: [(String, String)] -> String
+#if __GLASGOW_HASKELL__ < 900
+import           Config as GHC
+#else
+import           GHC.Settings.Config as GHC
+#endif
+
+import           Interpreter (ghc)
+
+version :: String
+#ifdef CURRENT_PACKAGE_VERSION
+version = CURRENT_PACKAGE_VERSION
+#else
+version = "unknown"
+#endif
+
+ghcVersion :: String
+ghcVersion = GHC.cProjectVersion
+
+versionInfo :: String
+versionInfo = unlines [
+    "doctest version " ++ version
+  , "using version " ++ ghcVersion ++ " of the GHC API"
+  , "using " ++ ghc
+  ]
+
+info :: String
+info = formatInfo $
+    ("version", version)
+  : ("ghc_version", ghcVersion)
+  : ("ghc", ghc)
+  : ghcInfo
+
+type Info = [(String, String)]
+
+ghcInfo :: Info
+ghcInfo = read $ unsafePerformIO (readProcess ghc ["--info"] "")
+
+formatInfo :: Info -> String
 formatInfo xs = " [" ++ (intercalate "\n ," $ map show xs) ++ "\n ]\n"
