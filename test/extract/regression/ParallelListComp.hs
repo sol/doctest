123253fe157252e779c180e26d52c9962ed7c107
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-20 14:05:26 +0800

    Delete test directory

--- test/extract/regression/ParallelListComp.hs
+++
@@ -1,5 +0,0 @@
-{-# LANGUAGE ParallelListComp #-}
-module ParallelListComp where
-
-foo :: [Int]
-foo = [x+y | x <- [1,2,3] | y <- [4,5,6]]
--- test/extract/type-class/Foo.hs
+++
@@ -1,7 +0,0 @@
-module Foo where
-
-
-class ToString a where
-
-  -- | Convert given value to a string.
-  toString :: a -> String
