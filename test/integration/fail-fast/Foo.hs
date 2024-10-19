4ecd2aae8918ad2e3ed48a52cec66777438ceeb2
Simon Hengel <sol@typeful.net>
2024-10-19 08:33:19 +0700

    Update Foo.hs

--- test/integration/fail-fast/Foo.hs
+++ test/integration/fail-fast/Foo.hs
@@ -3,18 +3,21 @@ module Foo where
 import Bar
 
 -- | A passing example
+--
 -- >>> 23
 -- 23
 test1 :: a
 test1 = undefined
 
 -- | A failing example
+--
 -- >>> 23
 -- 42
 test2 :: a
 test2 = undefined
 
 -- | Another passing example
+--
 -- >>> 23
 -- 23
 test3 :: a
